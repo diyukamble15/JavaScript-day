b7462c78daa5cf0c1884106664ceba77b5cb8a3e
diyukamble15 <diyukamble1506@gmail.com>
2026-01-07 19:22:01 +0530

    add remaining page

--- Day9,10/script.js
+++ Day9,10/script.js
@@ -1,1 +1,15 @@
 alert("Hello, World");
+
+// let title = document.title;
+// console.log("Title of the document is: " + title);   
+
+let number = parseInt(prompt("Enter a number: "));
+
+document.title = "Number is " + number;
+
+for (let i = 1; i <= 10; i++) {
+
+    let op = i * number;
+
+    document.writeln(op + "<br>");
+}
